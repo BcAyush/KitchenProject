72c92e9d7a7993fc25cb47a57186e1a3ef298d9f
BcAyush <ayushbc9@gmail.com>
2025-04-13 13:17:36 -0400

    Scripted walking and idle animations

--- kitchenproject/Player/Scripts/Player.cs
+++ kitchenproject/Player/Scripts/Player.cs
@@ -3,18 +3,31 @@ using Godot;
 using System;
 using System.Data;
 using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.Serialization;
 
 public partial class Player : CharacterBody2D
 {
-    [Export] public int move_speed = 100;
+    //initializes variables
+    [Export] public int move_speed = 75;
+    private AnimationTree _animTree;
+    private AnimationNodeStateMachinePlayback _stateMachine;
+
+    //Initializes nodes
+    public override void _Ready()
+    {
+        _animTree = GetNode<AnimationTree>("AnimationTree");
+        _animTree.Active = true;
+
+        _stateMachine = (AnimationNodeStateMachinePlayback)_animTree.Get("parameters/playback");
+    }
 
     public override void _PhysicsProcess(double delta)
     {
-        
+        //initializes input direction
         Vector2 inputDirection = Vector2.Zero;
 
-
+        //determines which direction to move the character
         if (Input.IsActionPressed("up"))
         {
             inputDirection.Y -= 1;
@@ -29,9 +42,23 @@ public partial class Player : CharacterBody2D
             inputDirection.X += 1;
         }
 
-        inputDirection = inputDirection.Normalized();
+        //inputDirection = inputDirection.Normalized();
+
+
 
         Velocity = inputDirection * move_speed;
+        if(inputDirection != Vector2.Zero)
+        {
+            _stateMachine.Travel("Walk");
+            _animTree.Set("parameters/Walk/blend_position", inputDirection);
+            _animTree.Set("parameters/Idle/blend_position", inputDirection);
+        }
+        else
+        {
+            _stateMachine.Travel("Idle");
+
+        }
+
         MoveAndSlide();
 
     }
